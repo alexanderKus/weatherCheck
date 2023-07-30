fe067c81c6e5c36ab3ec222e8efb232d11d61fe3
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-07-30 20:57:15 +0200

    Project structure fix

---
+++ Weather/src/Api/Controllers/WeatherController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Commands;
+using Application.Interfaces;
+using Domain.Common;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public WeatherController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost(Name = "GetWeatherForLocation")]
+    public async Task<ActionResult<WeatherStatistics>> GetWeatherForLocation([FromBody]Coordinates coordinates)
+    {
+        WeatherStatisticsCommand command = new(coordinates);
+        var response = await _mediator.Send(command);
+        return Ok(response);
+    }
+}
