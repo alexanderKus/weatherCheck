7d98f191b0738b2beb5183374be496fff9a958bc
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-07-31 19:20:29 +0200

    Update angular routing

--- Weather-Client/src/app/app-routing.module.ts
+++ Weather-Client/src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WeatherComponent } from './feature/weather/weather.component';
 
-const routes: Routes = [{ path: '', component: WeatherComponent }];
+const routes: Routes = [
+  { path: '', component: WeatherComponent },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
